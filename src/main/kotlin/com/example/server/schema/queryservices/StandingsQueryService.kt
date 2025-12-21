19a180cfd2a519a0e0a55ce2fa6a120a7e89617e
Mikkel Møller Jensen <mikkj17@gmail.com>
2025-12-21 16:35:33 +0100

    make pit stops, races and standings non-nullable (#7)

--- src/main/kotlin/com/example/server/schema/queryservices/StandingsQueryService.kt
+++ src/main/kotlin/com/example/server/schema/queryservices/StandingsQueryService.kt
@@ -6,22 +6,25 @@ import com.example.server.schema.models.standings.DriverStandingList
 import com.example.shared.mappers.toConstructorStandingList
 import com.example.shared.mappers.toDriverStandingList
 import com.expediagroup.graphql.server.operations.Query
+import io.ktor.server.plugins.*
 
 class StandingsQueryService(
     private val jolpicaClient: JolpicaClient
 ) : Query {
-    private val _driverCache = mutableMapOf<Pair<Int, Int?>, DriverStandingList?>()
-    private val _constructorCache = mutableMapOf<Pair<Int, Int?>, ConstructorStandingList?>()
+    private val _driverCache = mutableMapOf<Pair<Int, Int?>, DriverStandingList>()
+    private val _constructorCache = mutableMapOf<Pair<Int, Int?>, ConstructorStandingList>()
 
     suspend fun driverStandings(year: Int, round: Int? = null) = _driverCache.getOrPut(year to round) {
         jolpicaClient
             .getDriverStandings(year, round)
             ?.toDriverStandingList()
+            ?: throw NotFoundException("No driver standings found for $year, $round")
     }
 
     suspend fun constructorStandings(year: Int, round: Int? = null) = _constructorCache.getOrPut(year to round) {
         jolpicaClient
             .getConstructorStandings(year, round)
             ?.toConstructorStandingList()
+            ?: throw NotFoundException("No constructor standings found for $year, $round")
     }
 }
