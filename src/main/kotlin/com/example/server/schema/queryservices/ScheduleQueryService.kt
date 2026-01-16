952df33578fecc4ebce6d7a56414ab8bb013a263
Mikkel Møller Jensen <mikkj17@gmail.com>
2026-01-16 21:44:45 +0100

    add schedule query

--- src/main/kotlin/com/example/server/schema/queryservices/ScheduleQueryService.kt
+++ src/main/kotlin/com/example/server/schema/queryservices/ScheduleQueryService.kt
@@ -4,11 +4,21 @@ import com.example.client.jolpica.JolpicaClient
 import com.example.server.schema.models.schedule.Schedule
 import com.example.shared.mappers.toSchedule
 import com.expediagroup.graphql.server.operations.Query
+import io.ktor.server.plugins.*
 
 class ScheduleQueryService(
     private val jolpicaClient: JolpicaClient
 ) : Query {
-    private val cache = mutableMapOf<Triple<Int?, String?, String?>, List<Schedule>>()
+    private val _scheduleCache = mutableMapOf<Pair<Int, Int>, Schedule>()
+    private val _schedulesCache = mutableMapOf<Triple<Int?, String?, String?>, List<Schedule>>()
+
+    suspend fun schedule(year: Int, round: Int) = _scheduleCache.getOrPut(Pair(year, round)) {
+        jolpicaClient
+            .getSchedules(year, round)
+            .firstOrNull()
+            ?.toSchedule()
+            ?: throw NotFoundException("No schedule found for year $year and round $round")
+    }
 
     suspend fun schedules(
         year: Int? = null,
@@ -19,11 +29,11 @@ class ScheduleQueryService(
             "exactly one of year, driverId, or constructorId must be provided."
         }
 
-        return cache.getOrPut(
+        return _schedulesCache.getOrPut(
             Triple(year, driverId, constructorId)
         ) {
             val schedules = when {
-                year != null -> jolpicaClient.getSchedules(year)
+                year != null -> jolpicaClient.getSchedules(year, null)
                 driverId != null -> jolpicaClient.getSchedulesByDriver(driverId)
                 constructorId != null -> jolpicaClient.getSchedulesByConstructor(constructorId)
                 else -> throw AssertionError("unreachable")
